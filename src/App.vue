865a9909c24717c0b1fc346caa1987dec1e11c37
Simbiat Jinadu <160795172+Simby1@users.noreply.github.com>
2026-01-07 22:46:21 +0100

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -3,8 +3,7 @@
 <template>
   <h1>MaslaWay Launch DP Generator</h1>
   <p>
-    Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
-    documentation
+   Generate DP
   </p>
 </template>
 
