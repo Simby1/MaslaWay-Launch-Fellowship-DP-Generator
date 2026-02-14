6fd6636d0eee24149ecf3b201f72b17b091f14c7
Simby1 <kimsimby@gmail.com>
2026-02-14 23:57:19 +0100

    feat: today's commit

--- src/App.vue
+++ src/App.vue
@@ -2,11 +2,11 @@
 
 <template>
   <h1>MaslaWay Launch DP Generator</h1>
-  <button>Generate Launch Fellowship DP</button>
+  <button>Generate Launch Fellowship DP </button>
 </template>
 
 <style scoped>
 body {
-  background-image: url('image-path.jpg');
+  background-image: url('image-path-1.jpg');
 }
 </style>
